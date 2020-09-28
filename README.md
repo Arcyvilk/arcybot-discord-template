4614f7ce71811aab6bb39709e61d4a4f051337b3
Arcyvilk <blazemakerrr@gmail.com>
2020-09-29 00:17:19 +0200

    merge coflicts 2

--- README.md
+++
@@ -1,8 +0,0 @@
-# Bot Arcytemplate
-
-This is a Discord Bot template created by Arcyvilk (ARCYVILK#6666 @ Discord). 
-It comes with initial MongoDB support, and you will need a Mongo database for it to work!
-
-## Documentation
-
-[Available here](http://arcybot.github.io/)!
--- lib/commands/reactions.ts
+++
@@ -1,9 +0,0 @@
-import Discord from 'discord.js';
-// import { CustomReaction, TextReaction } from './logic';
-
-export const Reaction: {
-	[key: string]: (msg: Discord.Message) => string | void;
-} = {
-	// example: (msg: Discord.Message) =>
-	// 	new CustomReaction(msg).execute(example, msg),
-};
